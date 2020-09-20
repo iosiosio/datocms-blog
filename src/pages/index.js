e26d014fff94786df0c4cabfefb1d343f6d60d1a
iosiosio <42870845+iosiosio@users.noreply.github.com>
2020-09-20 19:40:44 +0200

    change path to /blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,12 @@ const IndexPage = ({ data }) => (
       {data.allDatoCmsWork.edges.map(({ node: work }) => (
         <div key={work.id} className="showcase__item">
           <figure className="card">
-            <Link to={`/works/${work.slug}`} className="card__image">
+            <Link to={`/blog/${work.slug}`} className="card__image">
               <Img fluid={work.coverImage.fluid} />
             </Link>
             <figcaption className="card__caption">
               <h6 className="card__title">
-                <Link to={`/works/${work.slug}`}>{work.title}</Link>
+                <Link to={`/blog/${work.slug}`}>{work.title}</Link>
               </h6>
               <div className="card__description">
                 <p>{work.excerpt}</p>
