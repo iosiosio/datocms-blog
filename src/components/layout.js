9b69f5261e005cedad5b59a47c403849157e4550
iosiosio <42870845+iosiosio@users.noreply.github.com>
2020-09-21 12:38:59 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,53 @@ import '../styles/style.css'
 
 import "../styles/index.sass";
 
+const duration = 0.5
+
+const variants = {
+  initial: {
+    opacity: 0,
+  },
+  enter: {
+    opacity: 1,
+    transition: {
+      duration: duration,
+      delay: duration,
+      when: 'beforeChildren',
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: { duration: duration },
+  },
+}
+
+const Layout = ({ children, location }) => (
+  <>
+    <Nav />
+    <div
+      style={{
+        margin: `60px auto 0`,
+        maxWidth: 960,
+        padding: `1em`,
+      }}
+    >
+      <AnimatePresence>
+        <motion.main
+          key={location.pathname}
+          variants={variants}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+        >
+          {children}
+        </motion.main>
+      </AnimatePresence>
+    </div>
+  </>
+)
+
+export default 
+
 const TemplateWrapper = ({ children }) => {
   const [showMenu, setShowMenu] = useState(false);
   return (
