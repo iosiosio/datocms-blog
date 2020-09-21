9cd2cc0283ff7b32656ff84f3ce12b4db4aa8777
iosiosio <42870845+iosiosio@users.noreply.github.com>
2020-09-21 13:14:12 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,15 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-has-content, jsx-a11y/anchor-is-valid*/
+
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
+import { HelmetDatoCms } from "gatsby-source-datocms";
 import { motion, AnimatePresence } from 'framer-motion'
 import Nav from '../components/Nav'
 import '../styles/style.css'
+
+import "../styles/index.sass";
 
 const duration = 0.5
 
@@ -23,29 +31,111 @@ const variants = {
   },
 }
 
-const Layout = ({ children, location }) => (
-  <>
-    <Nav />
-    <div
-      style={{
-        margin: `60px auto 0`,
-        maxWidth: 960,
-        padding: `1em`,
-      }}
-    >
-      <AnimatePresence>
-        <motion.main
-          key={location.pathname}
-          variants={variants}
-          initial="initial"
-          animate="enter"
-          exit="exit"
-        >
-          {children}
-        </motion.main>
-      </AnimatePresence>
-    </div>
-  </>
-)
+const TemplateWrapper = ({ children }) => {
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <StaticQuery
+      query={graphql`
+        query LayoutQuery {
+          datoCmsSite {
+            globalSeo {
+              siteName
+            }
+            faviconMetaTags {
+              ...GatsbyDatoCmsFaviconMetaTags
+            }
+          }
+          datoCmsHome {
+            seoMetaTags {
+              ...GatsbyDatoCmsSeoMetaTags
+            }
+            introTextNode {
+              childMarkdownRemark {
+                html
+              }
+            }
+            copyright
+          }
+          allDatoCmsSocialProfile(sort: { fields: [position], order: ASC }) {
+            edges {
+              node {
+                profileType
+                url
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <div className={`container ${showMenu ? "is-open" : ""}`}>
+          <HelmetDatoCms
+            favicon={data.datoCmsSite.faviconMetaTags}
+            seo={data.datoCmsHome.seoMetaTags}
+          />
+          <div className="container__sidebar">
+            <div className="sidebar">
+              <h6 className="sidebar__title">
+                <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
+              </h6>
+              <div
+                className="sidebar__intro"
+                dangerouslySetInnerHTML={{
+                  __html:
+                    data.datoCmsHome.introTextNode.childMarkdownRemark.html
+                }}
+              />
+              <ul className="sidebar__menu">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+              </ul>
+              <p className="sidebar__social">
+                {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
+                  <a
+                    key={profile.profileType}
+                    href={profile.url}
+                    target="blank"
+                    className={`social social--${profile.profileType.toLowerCase()}`}
+                  >
+                    {" "}
+                  </a>
+                ))}
+              </p>
+              <div className="sidebar__copyright">
+                {data.datoCmsHome.copyright}
+              </div>
+            </div>
+          </div>
+          <div className="container__body">
+            <div className="container__mobile-header">
+              <div className="mobile-header">
+                <div className="mobile-header__menu">
+                  <button
+                    onClick={e => {
+                      e.preventDefault();
+                      setShowMenu(!showMenu);
+                    }}
+                  />
+                </div>
+                <div className="mobile-header__logo">
+                  <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
+                </div>
+              </div>
+            </div>
+            {children}
+          </div>
+        </div>
+      )}
+    />
+  );
+};
 
-export default Layout
+TemplateWrapper.propTypes = {
+  children: PropTypes.object
+};
+
+export default TemplateWrapper;
+/* eslint-enable jsx-a11y/anchor-has-content, jsx-a11y/anchor-is-valid*/
