126113c5e0e7c7f55c6f874f9efe019c4d904b21
iosiosio <42870845+iosiosio@users.noreply.github.com>
2020-09-21 13:04:13 +0200

    Update work.js

--- src/templates/work.js
+++ src/templates/work.js
@@ -3,7 +3,7 @@ import Slider from 'react-slick'
 import { HelmetDatoCms } from 'gatsby-source-datocms'
 import Img from 'gatsby-image'
 import { graphql } from 'gatsby'
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 
 export default ({ data }) => (
   <Layout>
